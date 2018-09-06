affb7d55da867b64480fef5ea6b81969c70c14a7
Hervé BRY <herve.bry@geneanet.org>
2018-09-06 14:58:50 +0200

    Custom config

--- config/config.default.php
+++ config/config.default.php
@@ -30,11 +30,11 @@ return array(
     // Profile 1 in 100 requests.
     // You can return true to profile every request.
     'profiler.enable' => function() {
-        return rand(1, 100) === 42;
+	return (php_sapi_name() !== 'cli');
     },
 
     'profiler.simple_url' => function($url) {
-        return preg_replace('/\=\d+/', '', $url);
+        return $_SERVER['HTTP_HOST'] . rawurldecode($url);
     },
 
     'profiler.options' => array(),
